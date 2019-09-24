14abb8df176e2237a45e33cfaeb013792d563426
jtokarowski <johnt3@bu.ecu>
2019-09-24 16:31:34 -0400

    split auth function into separate classe, removed bulk of params from main file, changed to app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import json
-from flask import Flask, request, redirect, g, render_template, jsonify
+from flask import Flask, request, redirect, render_template, jsonify
 import requests
 from urllib.parse import quote
 from pymongo import MongoClient
 from datetime import date
-from spotifyClient import SpotifyData, SpotifyAuth
+from spotifyClient import data, auth
 
 #grab date program is being run
 td = date.today()
@@ -16,26 +16,14 @@ client = MongoClient('localhost', 27017)
 #creates instance of app
 app = Flask(__name__)
 
-# Spotify URLS
-SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
-SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
-SPOTIFY_API_BASE_URL = "https://api.spotify.com"
-API_VERSION = "v1"
-SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
-
 # Server-side Parameters
 CLIENT_SIDE_URL = "http://127.0.0.1"
 PORT = 8000
-REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
-AUTHED_URL = "{}:{}/authed".format(CLIENT_SIDE_URL, PORT)
-REFRESH_URL = "{}:{}/refresh".format(CLIENT_SIDE_URL, PORT)
-SCOPE = "playlist-read-private"
-STATE = "" #Should create a random string generator here to make a new state for each request
 
 @app.route("/")
 def index():
     # Auth Step 1: Authorize Spotify User
-    u1 = SpotifyAuth() #no access token yet
+    u1 = auth()
     url = u1.auth_url
     return redirect(url)
 
@@ -43,7 +31,7 @@ def index():
 @app.route("/callback/q")
 def callback():
     # Auth Step 2: Requests refresh and access tokens
-    t1 = SpotifyAuth()
+    t1 = auth()
     newPage = t1.get_accessToken(request.args['code'])
     return redirect(newPage)
 
@@ -56,39 +44,43 @@ def authed():
     token_type = "Bearer" #always bearer, don't need to grab this each request
     expires_in = request.args["expires_in"]
 
-    #Auth Step 4: Refresh Token is used to get refreshed access token
-    refreshPage = "{}?refresh_token={}&access_token={}".format(REFRESH_URL, refresh_token, access_token)
-
-    p1 = SpotifyData(access_token)
-    userName = p1.profile()
+    p1 = data(access_token)
+    p2 = p1.profile()
+    userName = p2.get("userName")
+    refreshPage = "{}?refresh_token={}&access_token={}".format(p2.get("REFRESH_URL"), refresh_token, access_token)
 
     #set up db for user
     dbName = str(TODAY) + str(userName)
     db = client[dbName] # Creates db instance per user per date
-    #collection=db.test
-    #result = collection.insert_one({'name':'test'})
+
+    test = p1.userPlaylists()
+    print(test)
+
 
     return render_template("index.html", title='Authenticated', token=access_token, refresh=refresh_token, link=refreshPage, user=userName)
 
 @app.route("/refresh")
 def refresh():
 
-    r1 = SpotifyAuth()
+    r1 = auth()
     r2 = r1.get_refreshToken(request.args.get("refresh_token"))
     access_token = r2.get('refreshed_access_token')
     refresh_token = r2.get('refreshed_refresh_token')
     expires_in = r2.get('refreshed_expires_in')
 
-    p1 = SpotifyData(access_token)
-    userName = p1.profile()
-
-    refreshPage = "{}?refresh_token={}&access_token={}".format(REFRESH_URL, refresh_token, access_token)
+    p1 = data(access_token)
+    p2 = p1.profile()
+    userName = p2.get("userName")
+    refreshPage = "{}?refresh_token={}&access_token={}".format(p2.get("REFRESH_URL"), refresh_token, access_token)
 
     return render_template("index.html", title='Refreshed', token=access_token, refresh=refresh_token, link=refreshPage, user=userName)
     
 
+@app.route("/playlists")
+def playlists():
 
-    # # Use the access token to access Spotify API
+    return "ok"
+    # Use the access token to access Spotify API
     # #authorization_header = {"Authorization": "Bearer {}".format(access_token)}
     
     #return jsonify(access_token) #authorization_header
--- spotifyClient.py
+++ spotifyClient.py
@@ -3,20 +3,134 @@ import sys
 import requests
 import json
 import time
+from urllib.parse import quote
+from pymongo import MongoClient
+from datetime import date
+
+#import dev keys
+with open('config.json') as json_data_file:
+    configData = json.load(json_data_file)
+
+#grab the secret variables
+CLIENT_ID = configData["spotify"]["cid"]
+CLIENT_SECRET = configData["spotify"]["secret"]
 
 #URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
 SPOTIFY_API_BASE_URL = "https://api.spotify.com"
 API_VERSION = "v1"
 SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
 
-class Spotify:
-  def __init__(self, access_token):
-    self.access_token = access_token
+# Server-side Parameters
+CLIENT_SIDE_URL = "http://127.0.0.1"
+PORT = 8000
+REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
+AUTHED_URL = "{}:{}/authed".format(CLIENT_SIDE_URL, PORT)
+REFRESH_URL = "{}:{}/refresh".format(CLIENT_SIDE_URL, PORT)
+SCOPE = "playlist-read-private"
+STATE = "" #Should create a random string generator here to make a new state for each request
 
-  def profile(self):
-  	user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
-  	authorization_header = {"Authorization": "Bearer {}".format(self.access_token)}
-  	profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
-  	profile_data = json.loads(profile_response.text)
-  	userName = profile_data["display_name"]
-  	return userName
+#grab date program is being run
+td = date.today()
+TODAY = td.strftime("%Y%m%d") ##YYYYMMDD
+
+#set up db instance
+client = MongoClient('localhost', 27017)
+
+
+#construct auth request params
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+    # "state": STATE,
+    "show_dialog": "true",
+    "client_id": CLIENT_ID
+}
+
+class auth:
+    def __init__ (self):
+    	#step 1: Spotify User OK's application access
+        url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
+        auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+        self.auth_url = auth_url
+
+    def get_accessToken(self, code):
+        # Auth Step 2: Requests refresh and access tokens
+        auth_payload = {
+        "grant_type": "authorization_code",
+        "code": str(code),
+        "redirect_uri": REDIRECT_URI,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        }
+
+        # Auth Step 3: Tokens are Returned to Application
+        post_request = requests.post(SPOTIFY_TOKEN_URL, data=auth_payload)
+        #unpack token response
+        response_data = json.loads(post_request.text)
+        access_token = response_data["access_token"]
+        refresh_token = response_data["refresh_token"]
+        token_type = "Bearer" #always bearer, don't need to grab this each request
+        expires_in = response_data["expires_in"]
+        newPage = "{}?access_token={}&refresh_token={}&expires_in={}".format(AUTHED_URL, access_token, refresh_token, expires_in)
+        return newPage
+
+    def get_refreshToken(self, refresh_token):
+        
+        #Auth Step 4: get new token with the refresh token
+        refresh_payload = {
+        "grant_type": "refresh_token",
+        "refresh_token": str(refresh_token),
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        }
+
+        post_request_refresh = requests.post(SPOTIFY_TOKEN_URL, data=refresh_payload)
+        print(post_request_refresh)
+        refreshed_response_data = json.loads(post_request_refresh.text)
+        refreshed_access_token = refreshed_response_data["access_token"]
+        if refresh_token in refreshed_response_data: #sometimes it doesn't return new refresh token, catch this issue
+            refreshed_refresh_token = refreshed_response_data["refresh_token"]
+        else: #use old refresh token if that happens
+        	refreshed_refresh_token = refresh_token
+        refreshed_token_type = refreshed_response_data["token_type"]
+        refreshed_expires_in = refreshed_response_data["expires_in"]
+
+        refreshed_parameters = {
+        "refreshed_access_token": refreshed_access_token,
+        "refreshed_refresh_token": refreshed_refresh_token,
+        "expires_in": refreshed_expires_in,
+        }
+
+        return refreshed_parameters
+
+
+class data:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def profile(self):
+        user_profile_api_endpoint = "{}/me".format(SPOTIFY_API_URL)
+        authorization_header = {"Authorization": "Bearer {}".format(self.access_token)}
+        profile_response = requests.get(user_profile_api_endpoint, headers=authorization_header)
+        profile_data = json.loads(profile_response.text)
+        userName = profile_data["display_name"]
+
+        response = {
+        "userName": userName,
+        "REFRESH_URL": REFRESH_URL
+        }
+
+        return response
+
+    def userPlaylists(self):
+        user_playlist_api_endpoint = "{}/me/playlists".format(SPOTIFY_API_URL)
+        authorization_header = {"Authorization": "Bearer {}".format(self.access_token)}
+        response = requests.get(user_playlist_api_endpoint, headers=authorization_header)
+        response_data = json.loads(response.text)
+
+        #### UNPACK THIS RESPONSE, RETURN KEY COMPONENTS
+
+        return response_data
