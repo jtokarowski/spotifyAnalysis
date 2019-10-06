0e7e74745711111b5aa58461e2793c605e626be6
jtokarowski <johnt3@bu.ecu>
2019-10-06 15:39:50 -0400

    got the pandas dataframe import running

--- regressionAnalysis.py
+++ regressionAnalysis.py
@@ -13,42 +13,51 @@ name = '20191005jtokarowski'
 db = client[name] #this repo won't change, ok to run tests here
 #collection = db.allSongInfo
 cursor = db.allSongInfo.find({})  #temp DB that has all songs in it
+
+#blank list for unpacked songs
+songs = []
+
 for document in cursor:
-	print(document)
-	input('waiting')
+
+    doc = document['audioFeatures']["audio_features"][0]
+
+    if doc==None:
+        continue
+
+    an = document['artistNames']
+    ai = document['artistIds']
+
+    if len(an)==1:
+    	document['artistNames'] = an[0]
+    else:
+        strList = ",".join(an)
+        document['artistNames'] = strList
+
+    if len(ai)==1:
+    	document['artistIds'] = ai[0]
+    else:
+        strList = ",".join(ai)
+        document['artistIds'] = strList
+
+    for key,val in doc.items():
+        document[str(key)] = val
+
+    del document['audioFeatures']
+    del document['type']
+    del document['_id']
+    del document['track_href']
+    del document['id']
+    del document['analysis_url']
+    del document['uri']
+    songs.append(document)
+
+
+df = pd.read_json(json.dumps(songs) , orient='records')
+
+print(df.head())
 
 # need to import json into pandas dataframe
 # need to merge duplicates, set playlist inclusion to be a list
-
-#{ "_id" : ObjectId("5d98f7bb9fbf27e2f4ab6663"),
-# "trackName" : "IBetMyLife-AlexAdairRemix",
-# "trackId" : "3bnEhDlFFXeCyyYhbRszW7",
-# "artistNames" : [ "Imagine Dragons" ],
-# "artistIds" : [ "53XhwfbYqKCa1cC15pYq2q" ],
-# "collection" : "DbMajorSet5.17",
-# "audioFeatures" : { "audio_features" : [ { "danceability" : 0.643, "energy" : 0.504, "key" : 1, "loudness" : -9.082, "mode" : 1, "speechiness" : 0.276, "acousticness" : 0.0809, "instrumentalness" : 0.000207, "liveness" : 0.152, "valence" : 0.625, "tempo" : 117.016, "type" : "audio_features", "id" : "3bnEhDlFFXeCyyYhbRszW7", "uri" : "spotify:track:3bnEhDlFFXeCyyYhbRszW7", "track_href" : "https://api.spotify.com/v1/tracks/3bnEhDlFFXeCyyYhbRszW7", "analysis_url" : "https://api.spotify.com/v1/audio-analysis/3bnEhDlFFXeCyyYhbRszW7", "duration_ms" : 190280, "time_signature" : 4 } ] } }
-
-
-
-
-
-#initialize the blank lists for attribute values
-# energy = []
-# valence = []
-
-# #loop thru all collections, append the data to the master lists
-# iterator = 0
-# for collection in collections:
-# 	collection = db[collections[iterator]]
-# 	cursor = collection.find({})
-# 	#print(collections[iterator])
-# 	iterator += 1
-
-# 	for document in cursor:
-# 		energy.append(document['energy'])
-# 		valence.append(document['valence'])
-	
-
 
 # plt.scatter(energy, valence, color='C1')
 
@@ -57,15 +66,3 @@ for document in cursor:
 # plt.title("Simple Plot")
 # plt.show()
         
-
-#merge all playlists into 1 master repo, split the attributes into vectors
-#label each song with the playlist (or playlists) that contained it
-#run analysis to see the differences by playlist
-#perhaps color code the plotted points by playlist so i can visualize it
-
-
-#select the relevant info from each entry (specific attribute) then insert into new db that only stores attributes
-
-#k-means goes here
-
-#put k-means output into new DB
