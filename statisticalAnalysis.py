a7c7dfcb0aac7d8133c45a893e6c1af300f280bc
jtokarowski <johnt3@bu.ecu>
2019-12-05 16:08:37 -0500

    more unused package dependencies removed

--- statisticalAnalysis.py
+++ statisticalAnalysis.py
@@ -1,7 +1,5 @@
 from pymongo import MongoClient
 import json
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()  # for plot styling
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
